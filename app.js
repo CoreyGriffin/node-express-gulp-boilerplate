7b6747eda3a9a638c4782402fc9ed980e7849643
Corey Griffin <coreyjgriff@gmail.com>
2017-01-24 19:48:40 -0500

    re-structured layout

--- app.js
+++ app.js
@@ -1,24 +1,53 @@
 'use strict';
+const express = require('express'),
+    hbs = require('hbs'),
+    path = require('path'),
+    log = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser');
 
-let express = require('express'),
-    bodyParser = require('body-parser'),
-    path = require('path');
+//importing routes
+const index = require('./routes/index');
 
-const app = express();
+//setting port and initiating express app
+const port = process.env.PORT || 3000;
+let app = express();
 
-//Setting up MiddleWare for body-parser
+//setting up view engine for handlebars
+app.set('views', path.join(__dirname, 'views'));
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+
+// Middleware
+app.use(log('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname + 'public')));
 
-//Setitng up middleware for public folders - setting static path
-app.use(express.static(path.join(__dirname, 'public')));
+//use imported routes
+app.use('/', index);
 
-
-//setting up home route
-app.get('/', (req, res) => {
-    res.send('Hello World');
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-app.listen(3000, () => {
-    console.log('Server started on port 3000..');
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+});
+
+module.exports = app;
--- gulpfile.js
+++ gulpfile.js
@@ -1,120 +1,13 @@
 'use strict';
-const gulp = require('gulp');
-const browserSync = require('browser-sync');
-const nodemon = require('gulp-nodemon');
-const sass = require('gulp-sass');
-const useref = require('gulp-useref');
-const uglify = require('gulp-uglify');
-const gulpIf = require('gulp-if');
-const imageMin = require('gulp-imagemin');
-const cache = require('gulp-cache');
-const del = require('del');
-const runSequence = require('run-sequence');
-const autoprefixer = require('gulp-autoprefixer');
-const babel = require('gulp-babel');
-
-//nodemon integration by sokgo @ https://github.com/sogko/gulp-recipes
-
-//  A slight delay to reload browsers
-// connected to browser-sync after restarting nodemon
-let BROWSER_SYNC_RELOAD_DELAY = 500;
-
-gulp.task('default', function(){
-  //default task stuff will run by simply running "gulp"
-  runSequence(['sass','browserSync', 'watch'])
-});
-
-gulp.task('nodemon', (e) => {
-  let called = false;
-  return nodemon({
-    //nodemon our express Server
-    script: 'app.js',
-    //watch core server file(s) that require server reboot
-    watch: ['app.js']
-  }).on('start', () => {
-    //ensure server start only got called once
-    if (!called) { e(); }
-    called = true;
-  }).on('restart', () => {
-    //reload connected browsers after slight delay
-    setTimeout(() => {
-      browserSync.reload({
-        stream: false
-      });
-    }, BROWSER_SYNC_RELOAD_DELAY);
-  });
-});
-
-//live reload/ nodemon gulp task
-gulp.task('browserSync', ['nodemon', 'sass'], () => {
-  browserSync({
-    //inform browser-sync to proxy our expressjs app
-    proxy: 'http://localhost:3000',
-    //inform browser-sync to use the following port for proxied app
-    port: 4000,
-    //open the proxied app in google chrome
-    browser: ['google-chrome']
-  });
-});
-
-//task for converting preprocessor into css
-gulp.task('sass', () => {
-  return gulp.src('./public/styles/styles.scss')
-    .pipe(sass())
-    .pipe(autoprefixer({
-      browsers: ['last 5 versions'],
-      cascade: false
-    }))
-    .pipe(gulp.dest('./public/styles'))
-    .pipe(browserSync.reload({
-      stream: true
-    }))
-});
-
-//watching files for changes
-gulp.task('watch', ['browserSync', 'sass'], function(){
-  gulp.watch('public/styles/**', ['sass']);
-  gulp.watch('public/scripts/**/*.js', browserSync.reload);
-  gulp.watch('public/images/**/*.+(png|jpg|gif|svg)', browserSync.reload);
-  gulp.watch('public/*.html', browserSync.reload);
-});
+const gulp = require('gulp'),
 
 
-// ------- CODE OPTIMIZATION FOR PRODUCTION --------------
-//useref concatenates scripts in the correct order based on the order in html
-//uglify and gulp-if minfies only if its a JS file or CSS file
-gulp.task('useref', function(){
-  return gulp.src('public/*.html')
-    .pipe(useref())
-    //minfies only JS files
-    .pipe(gulpIf('*.js', uglify()))
-    .pipe(gulp.dest('dist'))
-});
-
-//using imagemin to optimize images
-gulp.task('images', function(){
-  return gulp.src('/public/images/**/*.+(png|jpg|gif|svg)')
-  //caching images that run through minification
-    .pipe(cache(imageMin({
-      interlaced: true
-    })))
-    .pipe(gulp.dest('dist/images'))
-});
-
-//copy font files to the dist folder
-gulp.task('fonts', function(){
-  return gulp.src('public/assets/fonts/**/*')
-  .pipe(gulp.dist('dist/fonts'))
-});
-
-//cleaning house
-gulp.task('clean:dist', function(){
-  return del.sync('dist');
-});
 
 
-//combining gulp tasks for PRODUCTION
-gulp.task('launch', function(callback){
-  runSequence('clean:dist',
-  ['sass','useref','images','fonts'],callback)
-});
+
+
+
+
+
+
+// setting up clean task
