590cbdcc2c08ab66832405e37c11fb35680950b8
Corey Griffin <coreyjgriff@gmail.com>
2017-01-24 23:47:44 -0500

    added browsersync and nodemon for live reload

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,20 @@ const gulp = require('gulp'),
       runSequence = require('run-sequence'),
       sourcemaps = require('gulp-sourcemaps'),
       sass = require('gulp-sass'),
+      autoprefixer = require('gulp-autoprefixer'),
       rename = require('gulp-rename'),
       uglify = require('gulp-uglify'),
-      minifyCSS = require('gulp-clean-css');
+      minifyCSS = require('gulp-clean-css'),
+      nodemon = require('gulp-nodemon'),
+      browserSync = require('browser-sync');
 
 
 //default gulp task run with "gulp"
-gulp.task('default', () => {
-      runSequence(['sass'])
+gulp.task('default', ['sass', 'browserSync'], () => {
+  gulp.watch('public/stylesheets/**', ['sass']);
+  gulp.watch('public/scripts/**/*.js', browserSync.reload);
+  gulp.watch('public/images/**/*.+(png|jpg|gif|svg)', browserSync.reload);
+  gulp.watch('public/*.html', browserSync.reload);
 });
 
 //Compile sass files into main.css
@@ -18,10 +24,36 @@ gulp.task('sass', () => {
       return gulp.src('public/stylesheets/main.scss')
       .pipe(sourcemaps.init())
       .pipe(sass().on('error', sass.logError))
+      .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+        }))
       .pipe(sourcemaps.write('./maps'))
-      .pipe(gulp.dest('public/stylesheets'));
+      .pipe(gulp.dest('public/stylesheets'))
+      .pipe(browserSync.reload({
+      stream: true
+      }))
 });
 
+// //Browser-Sync for live reload
+gulp.task('browserSync', ['nodemon'], () => {
+  browserSync.init(null, {
+    proxy: 'http://localhost:3000',
+    port: 5000
+  });
+});
+
+gulp.task('nodemon', (cb) => {
+    let called = false;
+    return nodemon({script: 'app.js'}).on('start', () => {
+      if (!called) {
+        called = true;
+        cb();
+      }
+    });
+});
+
+//--------- RUN FOR PRODUCTION FILES ----------
 
 //Minify CSS and create main.min.css file for production build
 gulp.task('minifyCSS', () => {
@@ -33,3 +65,5 @@ gulp.task('minifyCSS', () => {
       .pipe(rename({ suffix: '.min'}))
       .pipe(gulp.dest('public/stylesheets'));
 });
+
+//Uglify JS task
