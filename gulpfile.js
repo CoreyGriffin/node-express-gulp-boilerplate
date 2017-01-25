20e2faef1b944ad8a21cdcf94ec3fcbbc139eb8a
Corey Griffin <coreyjgriff@gmail.com>
2017-01-24 22:01:09 -0500

    fixed file structure and added CSS minify task for production builds

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,11 @@
 const gulp = require('gulp'),
       runSequence = require('run-sequence'),
       sourcemaps = require('gulp-sourcemaps'),
-      sass = require('gulp-sass');
+      sass = require('gulp-sass'),
+      rename = require('gulp-rename'),
+      uglify = require('gulp-uglify'),
+      minifyCSS = require('gulp-clean-css');
+
 
 //default gulp task run with "gulp"
 gulp.task('default', () => {
@@ -15,5 +19,17 @@ gulp.task('sass', () => {
       .pipe(sourcemaps.init())
       .pipe(sass().on('error', sass.logError))
       .pipe(sourcemaps.write('./maps'))
+      .pipe(gulp.dest('public/stylesheets'));
+});
+
+
+//Minify CSS and create main.min.css file for production build
+gulp.task('minifyCSS', () => {
+      return gulp.src(['public/stylesheets/*.css', '!public/stylesheets/*.min.css'])
+      .pipe(minifyCSS({debug: true}, (details) => {
+            console.log('Original ' + details.name + ': ' + 'size: ' + details.stats.originalSize);
+            console.log('Minified ' + details.name + ': ' + 'size: ' + details.stats.minifiedSize);
+      }))
+      .pipe(rename({ suffix: '.min'}))
       .pipe(gulp.dest('public/stylesheets'));
 });
